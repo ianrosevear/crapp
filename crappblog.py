1650d19205d3e9fec225b34cd1fa9f7bc1ec9e4d
ianrosevear <ian.rosevear@gmail.com>
2022-01-31 17:05:27 -0500

    adding files

---
+++ crappblog.py
@@ -0,0 +1,37 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+posts = [
+    {
+        'author': 'Ian Rosevear',
+        'title': 'Blog Post 1',
+        'content': 'First post content',
+        'date_posted': 'April 20, 2018'
+    },
+    {
+        'author': 'Jack Rosevear',
+        'title': 'Blog Post 2',
+        'content': 'First post content',
+        'date_posted': 'April 21, 2018'
+    }
+]
+
+
+@app.route("/")
+@app.route("/home")
+def home():
+    return render_template('home.html', posts=posts)
+
+
+@app.route("/about")
+def about():
+    return render_template('about.html', title='About')
+
+
+
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
